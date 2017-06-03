396bef0b0fc3d6bcd03685fd21e65e86d8cbbecd
Andrea Bravo <andrea_bravoo@yahoo.es>
2017-06-03 18:10:20 +0200

    Saturday version

--- rover.js
+++ rover.js
@@ -4,102 +4,111 @@
 var myRover = {
   position: [0,0],
   direction: 'N'
-  direction: 'E'
-  direction: 'S'
-  direction: 'W'
-  // don't understand why "e" ?
 };
 
-//2 The Grid : space of movement
-// The grid is spherical (if I go to one edge, I come back to the initial one)
-// --> two dimensional arrays look in Google
+var rover = myRover;
 
+//2 Functions // define the movements of the bot
 
-// 2 ACTION //
-
-//3 Functions // define the movement
-//3.1 what is the movement
-
-//if (key is pressed) {
-// if (myRover.position == goForward)
-//  return move.myRover } else if {} [...]
-
-}
 function goForward(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.position[1]++
+      rover.position = [0,1];
+      //console.log(rover);
       break;
     case 'E':
-      rover.position[0]++
+      rover.position = [1,0];
       break;
     case 'S':
-      rover.position[0]--
+      //doc original está marcado 1
+      rover.position = [-0,-1];
       break;
     case 'W':
-      rover.position[0]--
+      rover.position = [-1,-0];
       break;
-  };
+  }
+}
 
 function goBackwards(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.position[0]++
+      rover.position = [-0,-1];
       break;
     case 'E':
-      rover.position[0]++
+      rover.position = [-1,-0];
       break;
     case 'S':
-      rover.position[1]--
+      rover.position = [0,1];
       break;
     case 'W':
-      rover.position[0]--
+      rover.position = [1,0];
       break;
-  };
+    }
+  }
 
 function goRight(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.position[0]++
+      rover.direction = "E";
       break;
     case 'E':
-      rover.position[1]++
+      rover.direction = "S";
       break;
     case 'S':
-      rover.position[0]--
+      rover.direction = "O";
       break;
     case 'W':
-      rover.position[0]--
+      rover.direction = "N";
       break;
-  };
+    }
+  }
 
 function goLeft(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.position[0]++
+      rover.direction = "O";
       break;
     case 'E':
-      rover.position[0]++
+      rover.direction = "N";
       break;
     case 'S':
-      rover.position[0]--
+      rover.direction = "E";
       break;
     case 'W':
-      rover.position[1]--
+      rover.direction = "S";
       break;
-  };
+    }
+  }
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
-}
+//3 Limits of the Grid: resetting the bot to the origin 0,0
 
-//3.2 functions to make the object move
-goForward(myRover);
-//function changeDirection();
-// the new changeDirection wil give a new value for the position = newPosition
-// f to go forward
-// b to go back
-// r to turn right
-// to turn left
+  //if(rover.position === [10,10]){
+  //  return rover.position === [0,0];
+  //} else if(rover.position === [-10,-10]){
+  //  return rover.position === [0,0];
+  //} else{}
 
-//4 Make the object move: call the functions
-// + track movement (display new position of the rover)
+
+//4 Define the movement of the bot
+
+var movement = "fffrfflfffbb";
+
+//function orders(movement){
+  for (var i=1 ; i<=movement.length ; i++) {
+    switch (movement[i]) {
+      case "r":
+        goRight(rover);
+        break;
+      case "l":
+        goLeft(rover);
+        break;
+      case "f":
+        goForward(rover);
+        break;
+      case "b":
+        goBackwards(rover);
+        break;
+      //default:
+    }
+    console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+  }
